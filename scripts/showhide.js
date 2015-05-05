5736f112a8b9f006a0da2a02e718f398fa8743d8
Kenny Wilson <kenny@digital-inflection.com>
2015-05-05 12:30:12 -0400

    initial commit

---
+++ scripts/showhide.js
@@ -0,0 +1,19 @@
+﻿
+		function ShowHide(btnID, divID)
+		{
+		    var btnElem = document.getElementById(btnID);
+		    var divElem = document.getElementById(divID);
+
+		    if(divElem.style.display === 'none')
+		    {
+		        btnElem.value = "Hide";
+
+		        divElem.style.display = 'block';
+		    }
+		    else
+		    {
+		        btnElem.value = "Show";
+
+		        divElem.style.display = 'none';
+		    }
+		}
